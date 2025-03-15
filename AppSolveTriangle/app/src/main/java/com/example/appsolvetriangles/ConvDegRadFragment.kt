3f30c0ae1c613e5308ba38020d2e0c262fd846f4
VanyaCoder232 <ivankrestyannikov2008@gmail.com>
2025-03-15 13:59:36 +0500

    Версия 15.03.25
    Задачи:
    1.Дописать фрагмент, решающий треугольники:
    1. 1. заполнение переменных в 26-27 нужными значениями,
    1.2 Создать блок кода, проверяющий введённые значения, решающий треугольник и выводящий новые значения.
    2. Поправить меню на маин активити, чтоб отображалось и не ругалось.
    3. Расписать поведение main activity
    4. Проверить ф-цию, проверяющую, простое ли число
    5. Решить проблему с иконками (белый фон)
    6. Вынести в отдельный файл ф-ции перевода градусов и рад.

--- AppSolveTriangle/app/src/main/java/com/example/appsolvetriangles/ConvDegRadFragment.kt
+++ AppSolveTriangle/app/src/main/java/com/example/appsolvetriangles/ConvDegRadFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.example.appsolvetriangles.databinding.FragmentConvDegRadBinding
 
 class ConvDegRadFragment : Fragment() {
-
+    lateinit var binding: FragmentConvDegRadBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,11 +19,29 @@ class ConvDegRadFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding.apply {
+            bRadToDeg.setOnClickListener{
+                InputDegs.setText(
+                    degrees(InputRads.getText().toString().toDouble()) .toString()
+                )
+            }
+            bDegToRad.setOnClickListener{
+                InputRads.setText(
+                    radians(InputDegs.getText().toString().toDouble()) .toString()
+                )
+            }
+        }
     }
     companion object {
         @JvmStatic
         fun newInstance() = ConvDegRadFragment()
 
+    }
+
+    fun degrees(rads:Double):Double {
+        return rads * 57.29577951308232
+    }
+    fun radians(degs:Double):Double{
+        return degs / 57.29577951308232
     }
 }
--- AppSolveTriangle/app/src/main/java/com/example/appsolvetriangles/SolveTriangleFragment.kt
+++ AppSolveTriangle/app/src/main/java/com/example/appsolvetriangles/SolveTriangleFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.appsolvetriangles.databinding.FragmentSolveTriangleBinding
-
+import kotlin.math.*
 
 class SolveTriangleFragment : Fragment() {
+    lateinit var binding: FragmentSolveTriangleBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,10 +21,137 @@ class SolveTriangleFragment : Fragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            bSolveIt.setOnClickListener {
+                var a:Double = 0.0; var b:Double = 0.0; var c:Double = 0.0;
+                var A:Double = 0.0; var B:Double = 0.0; var C:Double = 0.0;
+                
 
+            }
+        }
     }
     companion object {
         @JvmStatic
         fun newInstance() = SolveTriangleFragment()
+    }
+
+    fun degrees(rads:Double):Double {
+        return rads * 57.29577951308232
+    }
+    fun radians(degs:Double):Double{
+        return degs / 57.29577951308232
+    }
+
+    fun IsCorrectVars(_a:Double , _b:Double, _c: Double,
+                       _A:Double, _B:Double, _C:Double):Boolean{
+        var a = _a
+        var b = _b
+        var c = _c
+        var A = _A
+        var B = _B
+        var C = _C
+
+        binding.apply {
+            // Проверка на отрицательность и число введённых элементов
+            var counter = 0
+            for (element in listOf(a,b,c,A,B,C)){
+                if (element>0)
+                    counter++
+                else if (element<0){
+                    tVforErrs.text = "Элемент не может быть <0";return false}
+            }
+            if (counter<3){
+                tVforErrs.text = "Нужно минимум 3 стороны или угла";return false}
+
+            // Проверка на наличие И нормальный ввод сторон
+            if ((a==0.0) and (b==0.0) and (c==0.0)){
+                tVforErrs.text = "Нужна хотя бы 1 сторона";return false
+            }else if ( ((a+b<=c) or (a+c<=b) or (c+b<=a)) and
+                        ((a>0.0) and (b>0.0) and (c>0.0))
+                ){
+                tVforErrs.text = "2 стороны не могут быть равны или меньше третьей"
+                return false
+            }
+        }
+        return true
+    }
+
+    fun Solve_Triangle( _a:Double , _b:Double, _c: Double,
+                        _A:Double, _B:Double, _C:Double ) : Triangle{
+// Создание изменяемых переменных т.к. аргументы, переданные в ф-цию
+// Инициализируются как val
+
+        var a:Double = _a
+        var b:Double = _b
+        var c:Double = _c
+        var A:Double = radians(_A)
+        var B:Double = radians(_B)
+        var C:Double = radians(_C)
+
+//Особый порядок применения теорем для нахождения элементов
+        // 1. сторона по т. косинусов
+        if (a ==0.0 && A>0.0 && b>0.0 && c>0.0)
+            a=sqrt(b*b + c*c - 2*b*c*cos(A))
+        if (b ==0.0 && B>0.0 && a>0.0 && c>0.0)
+            b=sqrt(a*a + c*c - 2*a*c*cos(B))
+        if (c ==0.0 && C>0.0 && a>0.0 && b>0.0)
+            c=sqrt(a*a + b*b - 2*a*b*cos(C))
+
+        //2. Углы по т. косинусов
+        if (A==0.0 && B==0.0 && C==0.0 && a>0.0 && b>0.0 && c>0.0){
+            A=acos( (b*b + c*c - a*a) / (2*b*c) )
+            B=asin((b * sin(A)) / a)
+            C=PI - A - B
+            return Triangle(a,b,c, degrees(A),degrees(B),degrees(C))
+        }
+
+        //3. Углы по т. синусов
+        if (a>0.0 && A==0.0){
+            if (b>0.0 && B>0.0)
+                A=asin((a * sin(B)) / b)
+            else if (c>0.0 && C>0.0)
+                A=asin((a * sin(C)) / c)
+        }
+        if (b>0.0 && B==0.0){
+            if (a>0.0 && A>0.0)
+                B=asin((b * sin(A)) / a)
+            else if (c>0.0 && C>0.0)
+                B=asin((b * sin(C)) / c)
+        }
+        if (c>0.0 && C==0.0){
+            if (a>0.0 && A>0.0)
+                C=asin((a * sin(A)) / a)
+            else if (B>0.0 && b>0.0)
+                C=asin((c * sin(B)) / b)
+        }
+
+        //4. Углы по т. суммы углов
+        if (A>0.0 && B>0.0 && C==0.0)
+            C=PI - A - B
+        else if (A>0.0 && C>0.0 && B==0.0)
+            B=PI - A - C
+        else if (B>0.0 && C>0.0 && A==0.0)
+            A=PI - B - C
+
+        //5. стороны по т. синусов
+        if (a==0.0 && A>0.0){
+            if (b>0.0 && B>0.0)
+                a=sin(A) * b / sin(B)
+            else if (c>0.0 && C>0.0)
+                a=sin(A) * c / sin(C)
+        }
+        if (b==0.0 && B>0.0){
+            if (a>0.0 && A>0.0)
+                b=sin(B) * a / sin(A)
+            else if (c>0.0 && C>0.0)
+                b=sin(B) * c / sin(C)
+        }
+        if (c==0.0 && C>0.0){
+            if (a>0.0 && A>0.0)
+                c=sin(C) * a / sin(A)
+            else if (b>0.0 && B>0.0)
+                c=sin(C) * b / sin(B)
+        }
+        return Triangle(a,b,c, degrees(A),degrees(B),degrees(C))
     }
 }
